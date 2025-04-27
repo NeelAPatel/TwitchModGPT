d09615a43650126ce361042824fc5eb155b6e19f
Neel Patel <send2neelpatel@gmail.com>
2025-04-27 15:51:02 -0400

    Updated config to store title/category/emotes for streams

--- config.py
+++ config.py
@@ -2,20 +2,28 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+ENV_FILE = ".env"
 TWITCH_TOKEN = os.getenv("TWITCH_ACCESS_PREFIX_TOKEN")
 TWITCH_ACCESS_TOKEN = os.getenv("TWITCH_ACCESS_TOKEN")
 TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
-CHANNEL = os.getenv("TWITCH_CHANNEL_TO_ACCESS").lower()
 
 # ==== Execution configurator
 IGNORE_FILTER_FOR_USERS = {"streamelements", "fossabot", "streamlabs", "moobot", "nightbot"}
 
+# TWITCH_CHANNEL_TO_ACCESS="MichiMochievee"
+# CHANNEL = os.getenv("TWITCH_CHANNEL_TO_ACCESS").lower()
+CHANNEL = "ShyLily"
+CHANNEL_TITLE = ""
+CHANNEL_CATEGORY=""
+CHANNEL_EMOTE_LIST=""
+CHANNEL_EMOTE_PATTERN=""
+CHANNEL_GOAL_PROMPT=""
 # === LOCAL LLM CONFIG ===
 # LLM_MODEL = "deepseek-r1:7b"
 LLM_MODEL = "mistral:latest"
 
 LLM_LOGS_PATH = "logs/llm/"
-LLM_PROMPT_FILE = LLM_LOGS_PATH + "modgpt_prompt_log.log"
+LLM_PROMPT_FILE = LLM_LOGS_PATH + "modgpt_baseprompt.log"
 LLM_FILTERS_FILE = LLM_LOGS_PATH + "modgpt_filters_log.log"
 
 # ==== Terminal Configurator =======
@@ -28,3 +36,4 @@ EMOTES_FFZ_API_URL =f'https://emotes.crippled.dev/v1/channel/{CHANNEL}/ffz'
 EMOTES_BTTV_API_URL =f'https://emotes.crippled.dev/v1/channel/{CHANNEL}/bttv'
 EMOTES_TWITCH_API_URL =f'https://emotes.crippled.dev/v1/channel/{CHANNEL}/twitch'
 EMOTES_ALL_API_URL =f'https://emotes.crippled.dev/v1/channel/{CHANNEL}/all'
+
